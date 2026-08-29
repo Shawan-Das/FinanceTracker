1f78d65d8d9f1d003f57b4306d54783dad64a0e2
Shawan Das <86365217+Shawan-Das@users.noreply.github.com>
2026-08-30 00:14:57 +0600

    test deploye

--- FinanceTracker/api/index.js
+++ FinanceTracker/api/index.js
@@ -1,4 +1,11 @@
-const appModule = require('../server/dist/app.js');
-const app = appModule.default || appModule;
+let app;
 
-module.exports = app;
+try {
+  const appModule = require('../server/dist/app.js');
+  app = appModule.default || appModule;
+} catch (error) {
+  console.error('Failed to load the Express app for Vercel:', error);
+  throw error;
+}
+
+module.exports = (req, res, next) => app(req, res, next);
